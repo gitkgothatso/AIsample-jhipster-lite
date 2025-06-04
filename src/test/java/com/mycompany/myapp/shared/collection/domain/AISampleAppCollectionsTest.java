a748849228fc3ade1a0774e6bb41a9b84550f0e2
Kgothatso <k.seboya@gmail.com>
2025-06-04 09:26:49 +0200

    Apply module: java-base

---
+++ src/test/java/com/mycompany/myapp/shared/collection/domain/AISampleAppCollectionsTest.java
@@ -0,0 +1,115 @@
+package com.mycompany.myapp.shared.collection.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import com.mycompany.myapp.UnitTest;
+
+@UnitTest
+class AISampleAppCollectionsTest {
+
+  @Nested
+  @DisplayName("Collections")
+  class AISampleAppCollectionsCollectionsTest {
+
+    @Test
+    void shouldGetEmptyImmutableCollectionFromNullCollection() {
+      Collection<Object> input = null;
+      Collection<Object> collection = AISampleAppCollections.immutable(input);
+
+      assertThat(collection).isEmpty();
+      assertThatThrownBy(collection::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableCollectionFromMutableCollection() {
+      Collection<String> input = new ArrayList<>();
+      input.add("value");
+      Collection<String> collection = AISampleAppCollections.immutable(input);
+
+      assertThat(collection).containsExactly("value");
+      assertThatThrownBy(collection::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("Set")
+  class AISampleAppCollectionsSetTest {
+
+    @Test
+    void shouldGetEmptyImmutableCollectionFromNullCollection() {
+      Set<Object> input = null;
+      Set<Object> set = AISampleAppCollections.immutable(input);
+
+      assertThat(set).isEmpty();
+      assertThatThrownBy(set::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableCollectionFromMutableCollection() {
+      Set<String> input = new HashSet<>();
+      input.add("value");
+      Set<String> set = AISampleAppCollections.immutable(input);
+
+      assertThat(set).containsExactly("value");
+      assertThatThrownBy(set::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("List")
+  class AISampleAppCollectionsListTest {
+
+    @Test
+    void shouldGetEmptyImmutableCollectionFromNullCollection() {
+      List<Object> input = null;
+      List<Object> list = AISampleAppCollections.immutable(input);
+
+      assertThat(list).isEmpty();
+      assertThatThrownBy(list::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableCollectionFromMutableCollection() {
+      List<String> input = new ArrayList<>();
+      input.add("value");
+      List<String> list = AISampleAppCollections.immutable(input);
+
+      assertThat(list).containsExactly("value");
+      assertThatThrownBy(list::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("Map")
+  class AISampleAppMapTest {
+
+    @Test
+    void shouldGetEmptyImmutableMapFromNullMap() {
+      Map<Object, Object> input = null;
+      Map<Object, Object> map = AISampleAppCollections.immutable(input);
+
+      assertThat(map).isEmpty();
+      assertThatThrownBy(map::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableMapFromMutableMap() {
+      Map<String, String> input = new HashMap<>();
+      input.put("key", "value");
+      Map<String, String> map = AISampleAppCollections.immutable(input);
+
+      assertThat(map).containsExactly(Map.entry("key", "value"));
+      assertThatThrownBy(map::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+}
